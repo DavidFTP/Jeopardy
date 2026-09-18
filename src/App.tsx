d7ee83dddf326eda1dd5b34f185f6acf015b67f3
David <85558456+DavidFTP@users.noreply.github.com>
2026-09-18 18:05:51 +0300

    removing github pages files

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Play from "./pages/Play";
 
 export default function App() {
   return (
-    <BrowserRouter basename="/Jeopardy">
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/edit/:id" element={<Edit />} />
